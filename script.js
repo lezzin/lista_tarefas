7fdfe3bf86b69ecc9c7f331bee18a954ab891123
Leandro Adrian da Silva <leandrinsilva22@gmail.com>
2023-12-12 08:11:07 -0300

    feat: better readability to js code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const STORAGE_KEY = "todos";
+
 const forms = {
     add: document.querySelector("#add_form"),
     edit: document.querySelector("#edit_form"),
@@ -7,10 +9,10 @@ const forms = {
 };
 
 const todoList = document.querySelector(".todo");
-const todos = JSON.parse(localStorage.getItem("todos")) || [];
+let todos = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
 
 function saveStorage() {
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
 }
 
 function toggleForms() {
@@ -20,15 +22,23 @@ function toggleForms() {
 }
 
 function renderTodoItem(element, index) {
-    todoList.innerHTML += `
-      <li class="todo__item ${element.completed ? "completed" : "incompleted"}">
-          <p class="todo__item_description">${element.description}</p>
-          <div>
-              <button type="button" class="btn__check" onclick="changeTodoStatus(${index})"><i class="fa-solid fa-check"></i></button>
-              <button type="button" class="btn__pen" onclick="editTodo(${index})"><i class="fa-solid fa-pen"></i></button>
-              <button type="button" class="btn__xmark" onclick="removeTodo(${index})"><i class="fa-solid fa-xmark"></i></button>
-          </div>
-      </li>`;
+    const statusClass = element.completed ? "completed" : "incompleted";
+    const itemHtml = `
+        <li class="todo__item ${statusClass}">
+            <p class="todo__item_description">${element.description}</p>
+            <div>
+                <button type="button" class="btn__check" onclick="changeTodoStatus(${index})">
+                    <i class="fa-solid fa-check"></i>
+                </button>
+                <button type="button" class="btn__pen" onclick="editTodo(${index})">
+                    <i class="fa-solid fa-pen"></i>
+                </button>
+                <button type="button" class="btn__xmark" onclick="removeTodo(${index})">
+                    <i class="fa-solid fa-xmark"></i>
+                </button>
+            </div>
+        </li>`;
+    todoList.innerHTML += itemHtml;
 }
 
 function loadTodos() {
@@ -38,15 +48,19 @@ function loadTodos() {
 
 function editTodo(index) {
     toggleForms();
-    const todo = todos[index];
+    const { description } = todos[index];
     const input = forms.edit.querySelector("#edit_input");
     const inputHidden = forms.edit.querySelector("#edit_input_index");
-    input.value = todo.description;
+
+    input.value = description;
+    input.focus();
+
     inputHidden.value = index;
 }
 
 function changeTodoStatus(index) {
     todos[index].completed = !todos[index].completed;
+    saveStorage();
     loadTodos();
 }
 
@@ -56,45 +70,71 @@ function removeTodo(index) {
     loadTodos();
 }
 
-forms.add.addEventListener("submit", function (e) {
-    e.preventDefault();
+function handleAddTodo() {
     const input = forms.add.querySelector("#add_input");
     const value = input.value;
+
     if (value) {
         const todo = { description: value, completed: false };
         todos.push(todo);
-        input.focus();
+
         input.value = "";
+        input.focus();
+
         saveStorage();
         loadTodos();
     }
-});
+}
 
-forms.edit.addEventListener("submit", function (e) {
-    e.preventDefault();
+function handleEditTodo() {
     const newDescription = forms.edit.querySelector("#edit_input").value;
     const index = forms.edit.querySelector("#edit_input_index").value;
+
     if (newDescription) {
         todos[index].description = newDescription;
         loadTodos();
         saveStorage();
         toggleForms();
     }
-});
+}
 
-forms.search.querySelector("#search_input").addEventListener("input", function () {
-    const search = this.value.toLowerCase();
+function handleSearchTodo() {
+    const search = forms.search.querySelector("#search_input").value.toLowerCase();
+
     todoList.querySelectorAll(".todo__item").forEach((element) => {
-        element.classList.toggle("hidden", !element.querySelector(".todo__item_description").innerHTML.toLowerCase().includes(search));
+        const description = element.querySelector(".todo__item_description").innerHTML.toLowerCase();
+        element.classList.toggle("hidden", !description.includes(search));
     });
-});
+}
 
-forms.filter.querySelector("#filter_select").addEventListener("change", function (e) {
-    e.preventDefault();
-    const filter = this.value.toLowerCase();
+function handleFilterTodo() {
+    const filter = forms.filter.querySelector("#filter_select").value.toLowerCase();
+
     todoList.querySelectorAll(".todo__item").forEach((element) => {
         element.classList.toggle("hidden", !element.classList.contains(filter));
     });
-});
+}
 
+function addEventListeners() {
+    forms.add.addEventListener("submit", (e) => {
+        e.preventDefault();
+        handleAddTodo();
+    });
+
+    forms.edit.addEventListener("submit", (e) => {
+        e.preventDefault();
+        handleEditTodo();
+    });
+
+    forms.search.addEventListener("submit", (e) => {
+        e.preventDefault();
+        handleSearchTodo();
+    });
+
+    forms.search.querySelector("#search_input").addEventListener("input", handleSearchTodo);
+
+    forms.filter.querySelector("#filter_select").addEventListener("change", handleFilterTodo);
+}
+
+addEventListeners();
 loadTodos();
